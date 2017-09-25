14c968547fb75d455ad32c1e582333c051211253
Hugo Bessa <eu@hugobessa.com.br>
2017-09-25 19:53:35 -0300

    Use create-elm-app to build the app

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,19 +2,21 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
+
+
+-- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
 
 
 all : Test
 all =
     describe "A Test Suite"
         [ test "Addition" <|
-            \() ->
-                Expect.equal (3 + 7) 10
+            \_ ->
+                Expect.equal 10 (3 + 7)
         , test "String.left" <|
-            \() ->
+            \_ ->
                 Expect.equal "a" (String.left 1 "abcdefg")
         , test "This test should fail" <|
-            \() ->
+            \_ ->
                 Expect.fail "failed as expected!"
         ]
